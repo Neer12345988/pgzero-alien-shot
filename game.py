f4a55d3e3d849f4ac76308b9c85ce4e853016ca3
Neer12345988 <neer.saldanha@gmail.com>
2026-01-11 16:58:37 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ WIDTH = 500
 HEIGHT = 500
 TITLE = "Shoot the alien"
 
+change_color = False
 alien = Actor("alien")
 msg = ""
 
@@ -14,6 +15,10 @@ def draw():
 
     alien.draw()
     screen.draw.text(msg, center = (400, 20), fontsize = 30, color = "black")
+    if change_color:
+        change_colour()
+        alien.draw()
+        screen.draw.text(msg, center = (400, 20), fontsize = 30, color = "black")
 
 def update():
     pass
@@ -22,11 +27,20 @@ def place_alien():
     alien.x = randint(40, 450)
     alien.y = randint(50, 450)
 
+def change_colour():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(20, 255)
+    screen.clear()
+    screen.fill(color = (r, g, b))
+
 def on_mouse_down(pos):
-    global msg
+    global msg, change_color
     if alien.collidepoint(pos):
         place_alien()
         msg = "BAM!"
+        change_color = True
+
     else:
         msg = "Miss"
 
